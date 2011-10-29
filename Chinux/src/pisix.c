2b0e2f38620c99f1f9cf6ee5789c38d5162ea046
Federico Homovc <federicohomovc@gmail.com>
2011-10-29 16:39:08 -0300

    teta

--- Chinux/src/pisix.c
+++ Chinux/src/pisix.c
@@ -17,35 +17,11 @@
 #include "../include/process.h"
 #include "../include/defs.h"
 
-/*createProcessParam  myparam;*/
 
 extern int nextPID;
-/*extern int CurrentPID;
 
 
-int CreateProcessAt(char* name, int (*process)(int,char**), int tty, int argc, char** argv, int stacklength, int priority, int isFront)
-{
-	PROCESS * proc;
-	void * stack = malloc(stacklength);
-	proc = malloc(sizeof(PROCESS));
-	proc->name = (char*)malloc(15);
-	proc->pid = nextPID++;
-	proc->foreground = isFront;
-	proc->priority = priority;
-	memcpy(proc->name, name,str_len(name) + 1);
-	proc->state = READY;
-	proc->tty = tty;
-	proc->stacksize = stacklength;
-	proc->stackstart = (int)stack;
-	proc->ESP = LoadStackFrame(process,argc,argv,(int)(stack + stacklength - 1), end_process);
-	proc->parent = CurrentPID;
-	proc->waitingPid = 0;
-	proc->sleep = 0;
-	proc->acum = priority + 1;
-	set_Process_ready(proc);
-	return proc->pid;
-}
-*/
+
 
 void block_process(int pid){
 	_int_79_caller(BLOCK, pid);
